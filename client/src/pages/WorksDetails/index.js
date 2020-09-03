91cf2d67beb18ad8b632d139cd77a53dcb87750b
timsteeleallen <timsteeleallen@gmail.com>
2020-09-03 12:10:57 -0400

    fix header height. border-radius change

--- client/src/pages/WorksDetails/index.js
+++ client/src/pages/WorksDetails/index.js
@@ -1,108 +1,100 @@
-import React, { Component, } from "react";
-import { Card, ListGroup, ListGroupItem, Button, Image, CardDeck, Table, Accordion, Spinner, Row, Col } from 'react-bootstrap';
-import './style.css'
-import $ from 'jquery';
+import React, { Component } from "react";
+import {
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  Image,
+  CardDeck,
+  Table,
+  Accordion,
+  Spinner,
+  Row,
+  Col
+} from "react-bootstrap";
+import "./style.css";
+import $ from "jquery";
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // const queryString = require('query-string');
-import Navigation from "../../components/Navigation"
-import logo from '../../media/millie-logo-colored.png'
+import Navigation from "../../components/Navigation";
+import logo from "../../media/millie-logo-colored.png";
 require("dotenv").config();
 
 export default class WorksDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            chosenWork: ''
-        };
-    }
-
-
-     addSpaces =() => {
-        console.log("Adding spaces")
-        $('.modal-description').each(function () {
-          var text = $(this).text();
-          $(this).html(text.replace(/\*/g, '<br></br>'));
-        });
-      }
-
-    componentDidMount() {
-      
-
-        console.log(this.props.match.params)
-        let param = Object.values(this.props.match.params);
-        console.log(param)
-        this.setState({
-            chosenWork: param
-        })
-        // this.addSpaces();
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenWork: ""
     };
+  }
 
+  addSpaces = () => {
+    console.log("Adding spaces");
+    $(".modal-description").each(function() {
+      var text = $(this).text();
+      $(this).html(text.replace(/\*/g, "<br></br>"));
+    });
+  };
 
-    componentDidUpdate() {
-        this.addSpaces();
+  componentDidMount() {
+    console.log(this.props.match.params);
+    let param = Object.values(this.props.match.params);
+    console.log(param);
+    this.setState({
+      chosenWork: param
+    });
+    // this.addSpaces();
+  }
+
+  componentDidUpdate() {
+    this.addSpaces();
+  }
+
+  render() {
+    const { chosenWork, currentWork } = this.state;
+    const prevModal = this.props.worksObject[Number(chosenWork) - 1];
+    const thisModal = this.props.worksObject[chosenWork];
+    const nextModal = this.props.worksObject[Number(chosenWork) + 1];
+
+    if (thisModal) {
+      console.log(thisModal.additionalPics);
+      var addPics = thisModal.additionalPics.map((item, i) => (
+        <Image className="off-image-2" src={item.addPic} />
+      ));
     }
 
+    return (
+      <div>
+        <Navigation logo={logo} color="#FF8686" />
 
+        <div className="works-page">
+          {thisModal ? (
+            <div className="works-container">
+              <Image className="off-image" src={thisModal.image} />
+              <div className="works-description">
+                <h1 className="works-title">{thisModal.title}</h1>
+                {/* <hr style={{ width: `50%` }}></hr> */}
+                <p className="modal-description">{thisModal.description}</p>
+              </div>
+              {addPics}
+            </div>
+          ) : (
+            <div></div>
+          )}
 
-
-    render() {
-
-
-
-
-        const { chosenWork, currentWork } = this.state;
-        const prevModal = this.props.worksObject[Number(chosenWork) - 1]
-        const thisModal = this.props.worksObject[chosenWork];
-        const nextModal = this.props.worksObject[Number(chosenWork) + 1];
-
-        if (thisModal) {
-            console.log(thisModal.additionalPics);
-            var addPics = thisModal.additionalPics.map((item, i) =>
-            <Image className="off-image-2" src={item.addPic} />
-            )
-        }
-
-            
-
-
-
-        return (
+          <div className="nav-box">
             <div>
-               <Navigation logo={logo} color="#FF8686"/>
-
-            <div className="works-page">
-                {thisModal ? (
-                <div className="works-container">
-                    <Image className="off-image" src={thisModal.image} />
-                    <div className="works-description">
-                        <h1 className="works-title">{thisModal.title}</h1>
-                        {/* <hr style={{ width: `50%` }}></hr> */}
-                        <p className="modal-description">{thisModal.description}
-                        </p>
-                    </div>
-                    {addPics}
-                </div>
-                ) : (<div></div>)
-                }
-               
-                <div className="nav-box">
-                    <div>
-                        <a href="/">Back to Work</a>
-                    </div>
-                    <div>
-                        {prevModal &&
-                            <a href={`/works/${prevModal.id}`}>previous</a>}
-                        {prevModal && nextModal && <span> | </span>}
-                        {nextModal &&
-                            <a href={`/works/${nextModal.id}`}>next</a>}
-                    </div>
-                </div>
+              <a href="/">Back to Work</a>
             </div>
+            <div>
+              {prevModal && <a href={`/works/${prevModal.id}`}>previous</a>}
+              {prevModal && nextModal && <span> | </span>}
+              {nextModal && <a href={`/works/${nextModal.id}`}>next</a>}
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
