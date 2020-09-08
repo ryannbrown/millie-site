ec7d723cd5806061eb29c667c9087c24ab7149d7
timsteeleallen <timsteeleallen@gmail.com>
2020-09-07 21:37:58 -0400

    adding nav spacing, work/about

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,63 +1,57 @@
-import { Container, Nav, Button, Image, Row, Col } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar'
-import React, {Component} from 'react';
+import { Container, Nav, Button, Image, Row, Col } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import HomeHero from "../../components/HomeHero/index"
-import './style.css';
-import arrow from "../../media/white-arrow.png"
-import backgroundWoman from "../../media/background-woman.JPG"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft, faGlassMartini } from '@fortawesome/free-solid-svg-icons'
-
-
+import "./style.css";
+import arrow from "../../media/white-arrow.png";
+import backgroundWoman from "../../media/background-woman.JPG";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft, faGlassMartini } from "@fortawesome/free-solid-svg-icons";
 
 export default class Hero extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props);
-  
-      this.listener = null;
-      this.state = {
-        status: "top"
-      };
-    }
-  
-  
-    componentDidMount() {
-      this.listener = document.addEventListener("scroll", e => {
-        var scrolled = document.scrollingElement.scrollTop;
-        if (scrolled >= 120) {
-          if (this.state.status !== "bgChanged") {
-            this.setState({ status: "bgChanged" });
-          }
-        } else {
-          if (this.state.status !== "top") {
-            this.setState({ status: "top" });
-          }
+    this.listener = null;
+    this.state = {
+      status: "top"
+    };
+  }
+
+  componentDidMount() {
+    this.listener = document.addEventListener("scroll", e => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 120) {
+        if (this.state.status !== "bgChanged") {
+          this.setState({ status: "bgChanged" });
         }
-      });
-    }
+      } else {
+        if (this.state.status !== "top") {
+          this.setState({ status: "top" });
+        }
+      }
+    });
+  }
 
-    scrollDown = () => {
-      window.scrollTo(0, 850)
-    }
-  
-    componentDidUpdate() {
-      document.removeEventListener("scroll", this.listener);
-    }
-  
-    render() {
-  
-  
-  
-  
-  
-      return (
-      
+  scrollDown = () => {
+    window.scrollTo(0, 850);
+  };
+
+  componentDidUpdate() {
+    document.removeEventListener("scroll", this.listener);
+  }
+
+  render() {
+    return (
       <Row>
         {/* <Col md={3}></Col> */}
 
-        <Col sm={12} md={12} className="home-img" style={{
+        <Col
+          sm={12}
+          md={12}
+          className="home-img img-overlay"
+          style={{
             backgroundImage: `url(${backgroundWoman})`,
             backgroundColor: `#FF8686`,
             opacity: `90%`,
@@ -71,32 +65,27 @@ export default class Hero extends Component {
             alignItems: `center`,
             position: `relative`
             //   cover no-repeat center center fixed`
-        }}>
-            <Image className="centered-logo" src={this.props.logo}></Image>
-            <div className=" hero-text-block">
-                <h1 className="hero-header">{this.props.title}</h1>
-                <p className="hero-phrase">{this.props.paragraph1}</p>
-                <p className="hero-phrase">{this.props.paragraph2}</p>
-            </div>
+          }}
+        >
+          <Image className="centered-logo" src={this.props.logo}></Image>
+          <div className=" hero-text-block">
+            <h1 className="hero-header">{this.props.title}</h1>
+            <p className="hero-phrase">{this.props.paragraph1}</p>
+            <p className="hero-phrase">{this.props.paragraph2}</p>
+          </div>
 
-       <br></br>
-       {/* <div> */}
-      
-       <div className="arrow-container">
-            <a onClick={this.scrollDown}><Image className="arrow-img center" src={arrow}></Image></a>
-            </div>
-       {/* </div> */}
-         
-           
-      
-          
-            
+          <br></br>
+          {/* <div> */}
 
-
+          <div className="arrow-container">
+            <a onClick={this.scrollDown}>
+              <Image className="arrow-img center" src={arrow}></Image>
+            </a>
+          </div>
+          {/* </div> */}
         </Col>
         {/* <Col md={3}></Col> */}
-    </Row>
-      )
-  
-    }
+      </Row>
+    );
   }
+}
--- client/src/pages/WorksDetails/index.js
+++ client/src/pages/WorksDetails/index.js
@@ -10,7 +10,7 @@ import {
   Accordion,
   Spinner,
   Row,
-  Col,
+  Col
 } from "react-bootstrap";
 import "./style.css";
 import $ from "jquery";
@@ -27,13 +27,13 @@ export default class WorksDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chosenWork: "",
+      chosenWork: ""
     };
   }
 
   addSpaces = () => {
     console.log("Adding spaces");
-    $(".modal-description").each(function () {
+    $(".modal-description").each(function() {
       var text = $(this).text();
       $(this).html(text.replace(/\*/g, "<br><br>"));
     });
@@ -44,9 +44,8 @@ export default class WorksDetails extends Component {
     let param = Object.values(this.props.match.params);
     console.log(param);
 
-    
     this.setState({
-      chosenWork: param,
+      chosenWork: param
     });
     // this.addSpaces();
   }
@@ -60,25 +59,28 @@ export default class WorksDetails extends Component {
     const prevModal = this.props.worksObject[Number(chosenWork) - 1];
     const thisModal = this.props.worksObject[chosenWork];
     const nextModal = this.props.worksObject[Number(chosenWork) + 1];
-console.log("previous", prevModal)
-console.log("this", thisModal)
-console.log("next", nextModal)
+    console.log("previous", prevModal);
+    console.log("this", thisModal);
+    console.log("next", nextModal);
 
-if (prevModal) {
-    var prevId = Number(chosenWork) - 1
-}
-if (nextModal) {
-    var nextId = Number(chosenWork) + 1
-}
+    if (prevModal) {
+      var prevId = Number(chosenWork) - 1;
+    }
+    if (nextModal) {
+      var nextId = Number(chosenWork) + 1;
+    }
     if (thisModal && thisModal.imgs) {
       var addPics = thisModal.imgs.map((item, i) => (
-        <Image className="off-image-2" src={`https://millie-site.s3.amazonaws.com/${item}`} />
+        <Image
+          className="off-image-2"
+          src={`https://millie-site.s3.amazonaws.com/${item}`}
+        />
       ));
     }
 
     return (
       <div>
-         <Navigation
+        <Navigation
           color="#FF8686"
           scrolledBgColor="#FF8686"
           scrolledColor="white"
@@ -91,8 +93,12 @@ if (nextModal) {
           {thisModal ? (
             <div className="works-container">
               <Row>
-                <Col xs={{ span: 12, order: 2}} sm={{ span: 12, order: 2}} md={{ span: 6, order: 
-                'first'}} lg={{ span: 6, order: 'first'}}>
+                <Col
+                  xs={{ span: 12, order: 2 }}
+                  sm={{ span: 12, order: 2 }}
+                  md={{ span: 6, order: "first" }}
+                  lg={{ span: 6, order: "first" }}
+                >
                   <div className="off-image-container">
                     <Image
                       className="off-image img-responsive"
@@ -101,8 +107,11 @@ if (nextModal) {
                   </div>
                 </Col>
                 <Col
-                 xs={{ span: 12, order: 'first'}} sm={{span:12, order: 'first'}} md={{ span: 6, order: 2}} lg={{ span: 6, order: 2}}
-                 >
+                  xs={{ span: 12, order: "first" }}
+                  sm={{ span: 12, order: "first" }}
+                  md={{ span: 6, order: 2 }}
+                  lg={{ span: 6, order: 2 }}
+                >
                   <div className="works-description">
                     <h1 className="works-title">{thisModal.title}</h1>
                     {/* <hr style={{ width: `50%` }}></hr> */}
